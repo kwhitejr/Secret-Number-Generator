088642f787532ba2564d9ceb6cab9f5bab2e052b
kwhitejr <kwhitejr.edu@gmail.com>
2015-11-07 10:51:58 -1000

    Secret Number Generator all passed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,22 @@
 'use strict';
 module.exports = function() {
-  
+
+  var randomNumber = Math.floor(Math.random() * (1000001));
+  return function() {
+
+    // return the number, that was once random
+    return randomNumber;
+
+  };
 };
+
+/*
+Alternative method
+
+var secretNumber;
+
+return function() {
+  return secretNumber = secretNumber || Math.floor(Math.random() * 1000001);
+};
+
+*/
